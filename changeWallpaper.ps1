8fcf0e0556010581c33d49a5200fe966a2aaf2f0
MetitationDuck <dw9t5j9@gmail.com>
2020-10-11 15:21:22 +0900

    it works!!

---
+++ changeWallpaper.ps1
@@ -0,0 +1,16 @@
+﻿wget -Uri https://github.com/MeditationDuck/badusb/blob/main/image.jpg?raw=true -outfile $env:USERPROFILE\image.jpg
+
+
+
+Set-ItemProperty -path "HKCU:Control Panel\Desktop" -name wallpaper -value $env:USERPROFILE\image.jpg
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+rm $env:USERPROFILE\image.jpg
