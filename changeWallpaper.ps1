1d111da4498c422fe12e7e95768606ffdd0ee661
Naoki Yoshida <40266624+MeditationDuck@users.noreply.github.com>
2020-10-15 22:23:39 +0900

    add sleeptime

--- changeWallpaper.ps1
+++ changeWallpaper.ps1
@@ -1,6 +1,6 @@
 ﻿wget -Uri https://github.com/MeditationDuck/badusb/blob/main/image.jpg?raw=true -outfile $env:USERPROFILE\image.jpg
 
-
+Start-Sleep -s 5
 
 Set-ItemProperty -path "HKCU:Control Panel\Desktop" -name wallpaper -value $env:USERPROFILE\image.jpg
 rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
